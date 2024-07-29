1949dddcc9f612cf6bc5a600b2ec313c305c875b
Liliana Silva <lilianaods@outlook.com>
2024-07-29 19:14:43 -0400

    Add search

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: 'experimental',
-  },
+  // experimental: {
+  //   ppr: 'experimental',
+  // },
 };
 
 module.exports = nextConfig;
